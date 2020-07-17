e69f9726dd42918b48c761423c2c0e20a57cbf8e
jayden-zhihui <737043980@qq.com>
2020-07-17 13:48:36 +0800

    修改wrapper 链路追踪

---
+++ wrapper/tracer/http.go
@@ -0,0 +1,32 @@
+package tracer
+
+import (
+	"log"
+	"net/http"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+)
+
+// HTTPWrapper tracer wrapper
+func HTTPWrapper(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions {
+			return
+		}
+		spanCtx, _ := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(r.Header))
+		sp := opentracing.GlobalTracer().StartSpan(r.URL.Path, opentracing.ChildOf(spanCtx))
+		defer sp.Finish()
+
+		if err := opentracing.GlobalTracer().Inject(
+			sp.Context(),
+			opentracing.HTTPHeaders,
+			opentracing.HTTPHeadersCarrier(r.Header),
+		); err != nil {
+			log.Println(err)
+		}
+		ext.HTTPMethod.Set(sp, r.Method)
+		ext.HTTPUrl.Set(sp, r.URL.EscapedPath())
+		ext.HTTPStatusCode.Set(sp, http.StatusOK)
+	})
+}
